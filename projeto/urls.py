456eeb74358cb4959a65e8beaca242cd96ec7814
Mateus Chaves Fonseca <renatofonseca310@hotmail.com>
2022-10-09 10:14:52 -0300

    removi o contato e sobre

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# def home(request):
+    # return HttpResponse('Home')
+
+
+# Ao longo do tempo, vá tentando ler as funções e entender tudo certinho
+#  sobre parâmetros e tal. Lembre-se que Django é apenas Python
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls'))
 ]
