50a83b20b32ed1c3c936efb8d080c55cd385f848
Mateus Chaves Fonseca <renatofonseca310@hotmail.com>
2023-05-12 21:20:52 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -20,9 +20,15 @@ def recipe(request, id):
         })
 
 
-def category(request, id):
-    categories = Category.objects.all()
-    category = categories[id-1]
-    return render(request, 'recipes/pages/category-view.html', {
-        'category': category
-        })
+def category(request, category_id):
+    recipes = Recipe.objects.all().order_by('-id')
+    related = recipes.filter(category__id=category_id)
+    #return render(request, 'recipes/pages/category-view.html', {
+     #   'category': category
+      #  })
+
+    return render(request, 'recipes/pages/category-view.html', context={
+         'recipes': related,
+         'is_detail_page': False,
+         'category_id': category_id
+     })
