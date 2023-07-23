8a4b4f1ae7bd0f3c07c8ed713ddbb81d2cee7630
Mateus Chaves Fonseca <renatofonseca310@hotmail.com>
2023-07-22 22:23:26 -0300

    commit

--- recipes/views.py
+++ recipes/views.py
@@ -7,25 +7,39 @@ from .models import Category, Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id').filter(is_published=True)
-    return render(request, 'recipes/pages/home.html', context={
-         'recipes': recipes,
-         'is_detail_page': False,
-     })
+    recipes = Recipe.objects.all().order_by("-id").filter(is_published=True)
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "recipes": recipes,
+            "is_detail_page": False,
+        },
+    )
+
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
-    return render(request, 'recipes/pages/recipe-view.html', {
-        'recipe': recipe,
-        'is_detail_page': True
-        })
+    return render(
+        request,
+        "recipes/pages/recipe-view.html",
+        {"recipe": recipe, "is_detail_page": True},
+    )
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=category_id, is_published=True).order_by(
+            "-id"
+        )
+    )
 
-    return render(request, 'recipes/pages/category-view.html', context={
-         'recipes': recipes,
-         'is_detail_page': False,
-         'title': recipes[0].category.name
-     })
+    return render(
+        request,
+        "recipes/pages/category-view.html",
+        context={
+            "recipes": recipes,
+            "is_detail_page": False,
+            "title": recipes[0].category.name,
+        },
+    )
